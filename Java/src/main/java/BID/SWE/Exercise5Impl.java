bc88e6ffdc8e63813726641f8c82bf2c5632dde4
Fabian Geistert <fgeistert@gmail.com>
2019-12-12 16:50:13 +0100

    exercise 5

--- Java/src/main/java/BID/SWE/Exercise5Impl.java
+++ Java/src/main/java/BID/SWE/Exercise5Impl.java
@@ -1,10 +1,30 @@
 package BID.SWE;
 
-public class Exercise5Impl implements Exercise5
-{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.LinkedList;
 
+public class Exercise5Impl implements Exercise5 {
     @Override
-    public Object Method1(String s, int i) {
-        return null;
+    public LinkedList<String> Method1(String s, int i) {
+        LinkedList<String> list = new LinkedList<>();
+
+        try {
+            Socket socket = new Socket(s, i);
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                list.add(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
