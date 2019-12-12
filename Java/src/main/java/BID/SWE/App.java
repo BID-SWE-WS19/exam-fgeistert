babe9349d80b4c0d7b8915bbc5a98391e0d6b099
Fabian Geistert <fgeistert@gmail.com>
2019-12-12 13:53:25 +0100

    project setup

--- Java/src/main/java/BID/SWE/App.java
+++ Java/src/main/java/BID/SWE/App.java
@@ -1,15 +1,8 @@
 package BID.SWE;
 
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
 
